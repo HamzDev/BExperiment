79f07e00ff44b704dc5fff4db1eadfffc6ca9742
hamzDev <0963707@hr.nl>
2019-03-15 21:39:40 +0100

    Mail Connecting Contact fragment, Gradle Fix Up

--- app/src/main/java/com/main/bexperiment/fragments/ContactFragment.java
+++ app/src/main/java/com/main/bexperiment/fragments/ContactFragment.java
@@ -1,5 +1,6 @@
 package com.main.bexperiment.fragments;
 
+import android.net.http.SslCertificate;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.main.bexperiment.R;
+import com.main.bexperiment.contact.MailExecuter;
+import com.main.bexperiment.contact.MailInstance;
 
 /**
  * @author Hamza Fethi
  * @version 0.1
  */
 public class ContactFragment extends Fragment {
+
+    private Button sendButton;
+    private EditText subject, email, message;
 
     /**
      * Displays the given view on class call
@@ -26,6 +34,39 @@ public class ContactFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.contact_fragment, container, false);
+        View view = inflater.inflate(R.layout.contact_fragment, container, false);
+        sendButton = (Button) view.findViewById(R.id.sendEmailId);
+        subject = (EditText) view.findViewById(R.id.subjectId);
+        email = (EditText) view.findViewById(R.id.emailId);
+        message = (EditText) view.findViewById(R.id.messageId);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getRestrictions()) {
+                    MailInstance mailInstance = new MailInstance(subject.getText().toString(), message.getText().toString());
+                    new MailExecuter(v, mailInstance).send();
+                }
+            }
+        });
+        return view;
     }
+
+    /**
+     * Checks for restrictions
+     * @return
+     */
+    private boolean getRestrictions() {
+        if(!email.getText().toString().contains("@") || email.getText().toString().isEmpty()) {
+            System.out.println("Something went wrong with the email input!");
+            return false;
+        } else if (subject.getText().toString().isEmpty()) {
+            System.out.println("Something went wrong with the subject input");
+            return false;
+        } else if (message.getText().toString().isEmpty()) {
+            System.out.println("Something went wrong with the message input");
+            return false;
+        }
+        return true;
+    }
+
 }
