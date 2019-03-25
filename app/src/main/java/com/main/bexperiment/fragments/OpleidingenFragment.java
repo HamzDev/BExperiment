c79c7f8fd67c230a44a0484769e05a292e3f32a1
hamzDev <0963707@hr.nl>
2019-03-25 16:58:09 +0100

    Added data displaying for the listview

--- app/src/main/java/com/main/bexperiment/fragments/OpleidingenFragment.java
+++ app/src/main/java/com/main/bexperiment/fragments/OpleidingenFragment.java
@@ -1,5 +1,6 @@
 package com.main.bexperiment.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +8,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.main.bexperiment.R;
+import com.main.bexperiment.opleidingen.OpleidingenData;
+import com.main.bexperiment.opleidingen.Pop;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Hamza Fethi
  * @version 0.1
  */
 public class OpleidingenFragment extends Fragment {
+
+    private ListView listView;
+    private List<String> ITEMS = Arrays.asList("Informatica", "Media technolgy");
 
     /**
      * Displays the given view on class call
@@ -26,6 +38,25 @@ public class OpleidingenFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.opleidingen_fragment, container, false);
+        View view = inflater.inflate(R.layout.opleidingen_fragment, container, false);
+        listView = (ListView) view.findViewById(R.id.listView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_list_item_1, ITEMS);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                for(OpleidingenData opleidingenData : OpleidingenData.values()) {
+                    String name = opleidingenData.name().replace("_", " ");
+                    if(name.equalsIgnoreCase(ITEMS.get(position))) {
+                        Intent i = new Intent(view.getContext(), Pop.class);
+                        i.putExtra("opleiding-title", opleidingenData.name());
+                        i.putExtra("opleiding-date", opleidingenData.getDate());
+                        i.putExtra("opleiding-content", opleidingenData.getContent());
+                        view.getContext().startActivity(i);
+                    }
+                }
+            }
+        });
+        return view;
     }
 }
